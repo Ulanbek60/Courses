3fb445127c1b2e71aba44c2492a57d4da464b96f
Ulanbek60 <uislambekuulu@gmail.com>
2025-01-16 12:34:59 +0600

    i added req.txt and changed settings.py

--- Course/Course/settings.py
+++ Course/Course/settings.py
@@ -117,3 +117,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'course_app.UserProfile'
